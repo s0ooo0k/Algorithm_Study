9e93c19fd9e2d50efde8ef4f1073f46d1b48d805
s0ooo0k <izz_m2@naver.com>
2025-02-06 02:58:10 +0900

    [java] 파일 정리

--- java/Programmers/LV0/120806.java
+++ java/Programmers/LV0/120806.java
@@ -6,3 +6,12 @@ class Solution {
         return answer;
     }
 }
+
+/* 더 나은 풀이
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        return num1 * 1000 / num2;
+    }
+}
+*/
