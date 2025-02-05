6b3054855f55412fa26746daea7c25cc9ed34606
s0ooo0k <izz_m2@naver.com>
2025-02-06 02:43:40 +0900

    [docs] JAVA 폴더 정리

---
+++ java/Programmers/LV0/181936.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        if ((number%n==0) && (number%m==0))
+            answer=1;
+        else 
+            answer=0;
+        return answer;
+    }
+}
+
+// 삼항연산자 사용을 습관화하자!
+// return number % n == 0 && number % m == 0 ? 1 : 0;
