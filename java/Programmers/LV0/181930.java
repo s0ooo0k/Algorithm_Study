9cc20754ef5a414cb88787a1937ddfec21e5ee8b
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-02-19 09:42:22 +0900

    [java] 181930 주사위 게임 2

---
+++ java/Programmers/LV0/181930.java
@@ -0,0 +1,16 @@
+// 181930 주사위 게임 2
+
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        if (a==b && b==c && a==c){
+            return (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+        }
+        else if (a==b || b==c || a==c){
+            return (a+b+c)*(a*a+b*b+c*c);
+        }
+        else {
+            return a + b+ c;
+        }
+    }
+}
