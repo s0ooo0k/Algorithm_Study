8ab47e34e4c655450c7fed5ca46274fddeabf5a2
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-03-12 20:46:10 +0900

    Time: 1 ms (97.69%), Space: 45.5 MB (76.1%) - LeetHub

--- Leetcode/0605-can-place-flowers/0605-can-place-flowers.java
+++ Leetcode/0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,38 +1,24 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int cnt = 0;
+
+        // 그리디
+
         int len = flowerbed.length;
 
-        if (n==0) return true;
-        
-        if(len==1) {
-            if(flowerbed[0]==0) return n<=1;
-            return n==0;
-        }
+        for(int i=0; i<len; i++) {
+            if(flowerbed[i]==0) {
+                boolean left = (i==0 || flowerbed[i-1]==0); // 0이거나 비어있으면 true
+                boolean right = (i==len-1 || flowerbed[i+1]==0);
 
-        if(flowerbed[0]==0 && flowerbed[1]==0){
-            flowerbed[0]=1;
-            cnt++;
-            if(cnt==n) return true;
-        }
-
-        for(int i=1; i<len-1; i++){
-            if(flowerbed[i]==0 && flowerbed[i-1]==0 && flowerbed[i+1]==0){
-                flowerbed[i]=1;
-                cnt++;
-                if(cnt == n) return true;
-                i++;
+                if(left && right) {
+                    flowerbed[i]=1;
+                    n--;
+                    if(n==0) return true;
+                    i++;
+                }
             }
         }
-        
-        if(flowerbed[len-1]==0 && flowerbed[len-2]==0){
-            flowerbed[len-1]=1;
-            cnt++;
-
-            if(cnt == n) return true;
-
-        }
-        return false;
-
+            return n<=0;
     }
+
 }
