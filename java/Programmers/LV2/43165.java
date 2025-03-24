98153a09623de8eb989e604dc10fdb44fc587a4f
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-03-24 10:01:21 +0900

    [java] 43165 타겟 넘버

---
+++ java/Programmers/LV2/43165.java
@@ -0,0 +1,15 @@
+class Solution {
+    private int answer = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        return answer; 
+    }
+    public void dfs(int[] numbers, int target, int sum, int index) {
+        if(index==numbers.length) {
+            if(sum==target) answer++;
+            return ;
+        }     
+        dfs(numbers, target, sum+numbers[index], index+1);
+        dfs(numbers, target, sum-numbers[index], index+1);        
+    }
+}
