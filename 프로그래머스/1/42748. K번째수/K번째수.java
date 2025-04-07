a73523691c13594f11125c663f1bf40255ce8582
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-04-07 23:49:50 +0900

    [level 1] Title: K번째수, Time: 0.53 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,19 +1,33 @@
 import java.util.*;
 
+// class Solution {
+//     public int[] solution(int[] array, int[][] commands) {
+//         int[] answer = new int[commands.length];
+//         List<Integer> num = new ArrayList<>();
+        
+//         for(int i=0; i<commands.length; i++){
+//             for(int j=commands[i][0]-1; j<commands[i][1]; j++){
+//                 num.add(array[j]);
+//             }
+//             Collections.sort(num);
+//             answer[i]=num.get(commands[i][2]-1);
+//             num.clear();
+//         }
+        
+//         return answer;
+//     }
+// 
+
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        List<Integer> num = new ArrayList<>();
+public int[] solution(int[] array, int[][] commands) {
+    int[] answer = new int[commands.length];
         
-        for(int i=0; i<commands.length; i++){
-            for(int j=commands[i][0]-1; j<commands[i][1]; j++){
-                num.add(array[j]);
-            }
-            Collections.sort(num);
-            answer[i]=num.get(commands[i][2]-1);
-            num.clear();
-        }
+    for(int i=0; i<commands.length; i++) {
+        int[] tmp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+        Arrays.sort(tmp);
+        answer[i]=tmp[commands[i][2]-1];
+    }
         
-        return answer;
+    return answer;
     }
 }
