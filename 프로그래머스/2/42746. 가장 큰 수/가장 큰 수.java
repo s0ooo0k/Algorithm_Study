7419efeadd1fcca280b908d1c3134fbaa5fb6417
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-04-09 16:12:30 +0900

    [level 2] Title: 가장 큰 수, Time: 222.25 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,13 +1,13 @@
 import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
-        // 리스트 변환
+        // 문자열로 배열 저장
         String[] num = new String[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
             num[i] = Integer.toString(numbers[i]);
         }
         
-        // 절댓값 비교
+        // 순서 비교
         Arrays.sort(num, (a, b) -> (b+a).compareTo(a+b));
         if (num[0].equals("0")) return "0";
 
