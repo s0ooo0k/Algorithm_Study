c7486de8ecd9c9ea45ccdba00e5a5f7e0c3e4332
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-04-11 23:30:48 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.49 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int w, int num) {
+        int answer = 0;
+        int cnt = 1;
+        int boxX = -1, boxY = -1;
+        int h = (int) Math.ceil((double) n / w); 
+        int[][] boxlist = new int[h][w];
+
+        outerLoop:
+        for (int i = 0; i < h; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < w; j++) {
+                    boxlist[i][j] = cnt;
+                    if (cnt == n) break outerLoop;
+                    if (cnt == num) {
+                        boxX = i;
+                        boxY = j;
+                    }
+                    cnt++;
+                }
+            } else {
+                for (int j = w - 1; j >= 0; j--) {
+                    boxlist[i][j] = cnt;
+                    if (cnt == n) break outerLoop;
+                    if (cnt == num) {
+                        boxX = i;
+                        boxY = j;
+                    }
+                    cnt++;
+                }
+            }
+        }
+
+        for (int i = boxX; i < h; i++) {
+            if (boxlist[i][boxY] == 0) break;
+            answer++;
+        }
+
+        return answer;
+    }
+}
