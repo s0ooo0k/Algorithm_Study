7f0e065d6d8d156ba2a10f2e29c141368f097f41
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-05-01 23:55:26 +0900

    [Bronze V] Title: SciComLove, Time: 100 ms, Memory: 14028 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine()); 
+
+        if (n==1) {
+            System.out.println("SciComLove");
+        }
+
+        if(n==2) {
+            System.out.println("SciComLove");
+            System.out.println("SciComLove");
+        }
+
+        else {
+            System.out.println("SciComLove");
+        }
+        
+    }
+}
+
