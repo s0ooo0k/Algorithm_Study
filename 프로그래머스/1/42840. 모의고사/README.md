c7b4fa565bcc42f12521e1f7b0964531de8317f8
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-10-24 04:02:59 +0900

    [level 1] Title: 모의고사, Time: 2.93 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 9.18 MB, 시간: 6.66 ms
+메모리: 9.31 MB, 시간: 2.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 16일 23:46:12
+2025년 10월 24일 04:02:56
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,23 +1,24 @@
 def solution(answers):
-    a = [1, 2, 3, 4, 5] 
-    b = [2, 1, 2, 3, 2, 4, 2, 5] 
-    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] 
+    a = [1, 2, 3, 4, 5]
+    b = [2, 1, 2, 3, 2, 4, 2, 5]
+    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    scores = [0, 0, 0]
+    a_score, b_score, c_score = 0, 0, 0
     
     for i, answer in enumerate(answers):
-        if a[i % len(a)] == answer:
-            scores[0] += 1
-        if b[i % len(b)] == answer:
-            scores[1] += 1
-        if c[i % len(c)] == answer:
-            scores[2] += 1
+        if answer == a[i%len(a)]:
+            a_score+=1
+        if answer == b[i%len(b)]:
+            b_score+=1
+        if answer == c[i%len(c)]:
+            c_score+=1
+            
+    score = [a_score, b_score, c_score]
+    max_score = max(score)
     
-    max_score = max(scores)
-    
-    result = []
-    for i, score in enumerate(scores):
-        if score == max_score:
-            result.append(i + 1)
-    
-    return result
+    answer = []
+    for i in range(3):
+        if score[i] == max_score:
+            answer.append(i+1)
+            
+    return answer
