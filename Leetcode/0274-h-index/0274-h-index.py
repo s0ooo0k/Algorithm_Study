88fd3baf6268e21930eec9689e59ab6391961ec7
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-12-29 09:22:29 +0900

    Time: 3 ms (38.3%), Space: 12.7 MB (37.14%) - LeetHub

---
+++ Leetcode/0274-h-index/0274-h-index.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def hIndex(self, citations):
+        citations.sort(reverse=True)
+
+        h=0
+        for i in range(len(citations)):
+            if citations[i]>=i+1:
+                h = i+1
+        
+        return h
